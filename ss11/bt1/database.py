d1c71aa35b1ca0c575dfa7d38414a1ae8a1a3853
khoaset <khoaset695@gmail.com>
2026-07-09 07:36:54 +0700

    Update database.py

--- ss11/bt1/database.py
+++ ss11/bt1/database.py
@@ -1,2 +1,8 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "mysql+pymysql://root:binbi123#@localhost:3306/parking_db"
+
+engine = create_engine(DATABASE_URL, echo=True, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
