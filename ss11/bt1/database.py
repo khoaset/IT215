62e3997683971deb1cf9e1e3d74dfacc6dfcf463
khoaset <khoaset695@gmail.com>
2026-07-09 07:44:07 +0700

    Update database.py

--- ss11/bt1/database.py
+++ ss11/bt1/database.py
@@ -1,8 +1,22 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:binbi123#@localhost:3306/parking_db"
+DATABASE_URL = "mysql+pymysql://root:password@localhost/parking_db"
 
-engine = create_engine(DATABASE_URL, echo=True, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
